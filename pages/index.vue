dfa7aa6e170a5b463dcffa20a39f2cefd0fb63c1
Tom Armstrong <tarmstrong91@gmail.com>
2019-08-06 19:52:13 +0100

    First Test

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,51 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        beaumont-decorating-website
-      </h1>
-      <h2 class="subtitle">
-        Beaumont Decorating Brochure Website
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<div>
+<h1>Index</h1>
+<nuxt-link to="/about">About</nuxt-link>
+<button @click="toggle"
+        :class="classObject">Toggle Me</button>
+</div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
+
+  data() {
+    
+    return {
+      
+      classObject: {
+        blue: true
+      }
+      
+    }
+    
+  },
+  
+  methods: {
+    
+    toggle: function() {
+      this.classObject['blue'] = !this.classObject['blue'];  
+    }
+    
   }
+  
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  
+  button {
+    font-size: 48px;
+    padding: 50px;
+  }
+  
+  .blue {
+    background-color: blue;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
+
+
