6e0681a5975a1d352254bc76722dc362043e2a74
Tom Armstrong <tarmstrong91@gmail.com>
2019-08-06 19:38:37 +0100

    Init

---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+
+export default {
+  mode: 'universal',
+  generate: {
+    dir: 'dist'
+  },
+
+  router: {
+    base: process.env.DEPLOY_ENV === 'STATIC' ? '/beaumont-decorating-website/' : '/'
+  },
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  devModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    'kenticocloud-nuxt-module'
+  ],
+  kenticocloud: {
+    projectId: '0fb91298-5e41-003e-f2c1-e66741931430',
+    enableAdvancedLogging: false,
+    previewApiKey: ''
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
